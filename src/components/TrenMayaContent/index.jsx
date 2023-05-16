9353c549135cf7caf0e82a477939830d59e5dcbc
gthbdevweb <devweb180@gmail.com>
2023-05-15 20:28:56 -0600

    [ADDED] tran maya info

--- src/components/TrenMayaContent/index.jsx
+++ src/components/TrenMayaContent/index.jsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState, useContext } from 'react';
 import { LazyLoadImage } from 'react-lazy-load-image-component';
 import { Context } from '../../Context';
-import dataEs from '../../assets/data/description/es/index.json';
-import dataEn from '../../assets/data/description/en/index.json';
+import dataEs from '../../assets/data/trenMaya/es/index.json';
+import dataEn from '../../assets/data/trenMaya/en/index.json';
 import Share from '../Share';
 import './style.css';
 
@@ -18,23 +18,37 @@ const TrenMayaContent = () => {
 
   return (
     <section className="tren-maya-content-ctn">
-      <div className="tren-maya-content-ctn-text">
-        <h2 className="tren-maya-content-text-title">Tren Maya</h2>
-        <p className="tren-maya-content-text-desc">Próximamente</p>
+      <div className="tren-maya-content-description-ctn">
+        <div className="tren-maya-content-ctn-text">
+          <h1 className="tren-maya-content-text-title">{language?.title}</h1>
+          {language?.description?.map((info, index) => <p className="tren-maya-content-text-desc" key={index}>{info?.text}</p>)}
+          <Share 
+            urlFaceboock = "https://visitmexico.com/trenmaya/"
+            urlPinterest = "https://visitmexico.com/trenmaya/"
+            urlTwitter = "https://visitmexico.com/trenmaya/"
+            urlWhatsapp = "https://visitmexico.com/trenmaya/"
+          />
+        </div>
+        <div className="tren-maya-content-ctn-img">
+          <LazyLoadImage
+            className="tren-maya-content-img"
+            src={require(`../../assets/img/trenMaya/tren.webp`)} 
+            alt={language?.title}
+          />
+        </div>
       </div>
-      <div className="tren-maya-content-ctn-img">
-        <LazyLoadImage
-          className="tren-maya-content-img"
-          src={require(`../../assets/img/description/NAY_sanblas_playas_98.webp`)} 
-          alt={language?.title}
+      <div className="tren-maya-video-ctn">
+        <iframe
+          width="100%"
+          height="100%"
+          src={language?.urlVideo}
+          title="Conoce las Ciudades Mexicanas Patrimonio Mundial. Visit México."
+          frameBorder="1"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowFullScreen
+          loading='lazy'  
         />
       </div>
-      <Share 
-        urlFaceboock = "https://visitmexico.com/trenmaya/"
-        urlPinterest = "https://visitmexico.com/trenmaya/"
-        urlTwitter = "https://visitmexico.com/trenmaya/"
-        urlWhatsapp = "https://visitmexico.com/trenmaya/"
-      />
     </section>
   )
 };
