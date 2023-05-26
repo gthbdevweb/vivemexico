be5068c10a2e1124c9e48f1a47277467563c7eeb
gthbdevweb <devweb180@gmail.com>
2023-05-25 20:48:57 -0600

    [ADDED] video 360 with iframe solution

--- src/components/VideoHome/index.jsx
+++ src/components/VideoHome/index.jsx
@@ -19,10 +19,21 @@ const VideoHome = () => {
     <section className="video-home-ctn">
       <h2 className="video-home-text-title">{language.title}</h2>
       <div className="video-home-vide-ctn">
+        {/*<iframe
+          loading='lazy'
+          width="100%"
+          height="100%" 
+          src="https://www.youtube.com/embed/EjNe5EJ1cAQ?rel=0;&autoplay=0"
+          title="Conoce las Ciudades Mexicanas Patrimonio Mundial. Visit México."
+          frameBorder="1"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+          allowFullScreen
+        />*/}
         <iframe
           loading='lazy'
           width="100%"
-          height="100%" src="https://www.youtube.com/embed/EjNe5EJ1cAQ?rel=0;&autoplay=0"
+          height="100%" 
+          src="https://www.mexico360.com.mx/bajacalifornia/indexweb.html"
           title="Conoce las Ciudades Mexicanas Patrimonio Mundial. Visit México."
           frameBorder="1"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
